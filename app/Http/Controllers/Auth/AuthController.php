824c2c4a7caeccff085af7154367b904ea36cfc3
Kirill Litvinov <kirill.litvinov@nixsolutions.com>
2018-11-19 13:48:55 +0200

    Auth message updated

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -31,7 +31,11 @@ class AuthController extends Controller
         ]);
 
         if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['message' => trans('auth.failed')], 401);
+            return response()->json([
+                'message' => trans('auth.failed'),
+                'errors' => [
+                    'auth' => trans('auth.failed')]
+            ], 401);
         }
 
         return $this->respondWithToken($token);
