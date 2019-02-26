bdeaf8a783c5aca70c3af77c8c5f48b0e050a42f
Kirill Litvinov <kirill.litvinov@nixsolutions.com>
2019-02-26 17:28:55 +0200

    Fix infinite recursion

--- resources/js/pages/orders/OrderShow/OrderStatus.vue
+++ resources/js/pages/orders/OrderShow/OrderStatus.vue
@@ -1,22 +1,28 @@
 <template>
-    <v-menu>
-      <v-btn :color="order.status.color" slot="activator">{{ order.status.title }}</v-btn>
-      <v-list>
-        <v-list-tile :key="status.id" @click="showModal(status)" v-for="status in statuses">
-          <v-list-tile-title>{{ status.title }}</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-menu>
+  <v-menu>
+    <v-btn :color="order.status.color" slot="activator">{{ order.status.title }}</v-btn>
+    <v-list>
+      <v-list-tile :key="status.id" @click="showModal(status)" v-for="status in statuses">
+        <v-list-tile-title>{{ status.title }}</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-menu>
 </template>
 
 <script lang="ts">
   import {Component, Vue} from "vue-property-decorator";
+  import {OrderStatus as OrderStatusModel} from "../../../models/OrderStatus";
   import {ordersStore} from "../../../store/modules/OrdersStore";
 
   @Component
   export default class OrderStatus extends Vue {
     dialog: boolean = false;
-    newStatus: OrderStatus = new OrderStatus();
+
+    newStatus: OrderStatusModel = new OrderStatusModel();
+
+    created() {
+      console.log('created');
+    }
 
     get order() {
       return ordersStore.order;
@@ -26,7 +32,7 @@
       return ordersStore.statuses;
     }
 
-    showModal(status: OrderStatus) {
+    showModal(status: OrderStatusModel) {
       this.newStatus = status;
       this.dialog = true;
     }
